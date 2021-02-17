67101793f3dee9607046b1a9f099db4c76ddc19f
ayash12 <ayashabdus@gmail.com>
2021-02-17 13:59:39 +0700

    second commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <img class="img1" src="./assets/seedbg.png" alt="">
+    <img class="img2" src="./assets/wateringbg.png" alt="">
+    <img class="img3" src="./assets/seedbag.png" alt="" @click.prevent="$refs.modalName.openModal()">
+    <img class="img4" src="./assets/watering.png" alt="" >
+    <img class="img5" src="./assets/pot.png" alt="">
+    <modal ref="modalName">
+    </modal>
+    
+  <!-- <div>
+		<div id="login"><button onclick="login();">Please Login</button></div>
+		<div id="gotin" style="display:none;">
+			Welcome <b><span id="user"></span></b>
+			<br>
+			<button id="logout" onclick="logout();">Logout</button>
+		</div>
+	</div> -->
+
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal";
+
+export default {
+  name: "App",
+   components: {
+     Modal
+    }
+  
+};
+</script>
+
+<style>
+  @import './assets/style.css';
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,100 @@
+<template>
+  <mdb-container> 
+  <mdb-modal v-if="show" centered  :show ="modal" @close="show = false" scrollable>
+        <mdb-modal-header>
+            <mdb-modal-title>Buy Seeds</mdb-modal-title>
+        </mdb-modal-header>
+        <mdb-modal-body>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                <img src="@/assets/seedbag.png" alt="" style="width:100px">
+                <div class="container">
+                  vexamania
+                  <br>
+                  <h6> get 1120 vex </h6>
+                </div>
+                  <mdb-btn color="primary" size="sm" @click.native="buySeed()">1k vex</mdb-btn>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <img src="@/assets/seedbag.png" alt="" style="width:100px">
+                <div class="container">
+                  vexdefi
+                  <br>
+                  <h6> get 1100 vex </h6>
+                </div>
+                   <mdb-btn color="primary" size="sm" @click.native="buySeed()">1k vex</mdb-btn>
+                </div>
+              </div>
+            </div>
+          </div>            
+         
+        </mdb-modal-body>
+        <mdb-modal-footer>
+            <mdb-btn color="primary" size="sm" @click.native="show = false">Close</mdb-btn>
+        </mdb-modal-footer>
+    </mdb-modal>
+    <div></div>
+  </mdb-container>
+</template>
+
+<script>
+
+import { mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter, mdbBtn, mdbRow, mdbCol, mdbContainer } from 'mdbvue';
+
+export default {
+  name: "Modal",
+   components: {
+      mdbModal,
+      mdbModalHeader,
+      mdbModalTitle,
+      mdbModalBody,
+      mdbModalFooter,
+      mdbBtn,
+      mdbRow, 
+      mdbCol,
+      mdbContainer
+    },
+  data() {
+    return {
+      show: false,
+      counter: 0
+    };
+    
+  },
+  methods: {
+    openModal() {
+      this.show = !this.show;
+      },
+    buySeed() {
+     this.counter++;
+      // this.show = false;
+      console.log({counter});
+      }
+  }
+};
+</script>
+
+
+<style >
+.col-sm-6 {
+  width: 50%;
+  background-color: white;
+  margin-bottom: 10px;
+}
+
+.container {
+  text-align: center;
+  padding: 10px 10px;
+}
+
+h6 {
+  font-size: 13px;
+}
+
+</style>
